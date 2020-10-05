c8fa6871996e030cbf5e58569104c77f629ba324
abs  kawser <abskawser77000@gmail.com>
2020-10-05 23:12:48 +0600

    almost done the project

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDijw-CGYR_KZCVGQcdySMiTnAThu_ucNM",
+  authDomain: "volunteer-client-private-mast.firebaseapp.com",
+  databaseURL: "https://volunteer-client-private-mast.firebaseio.com",
+  projectId: "volunteer-client-private-mast",
+  storageBucket: "volunteer-client-private-mast.appspot.com",
+  messagingSenderId: "682393464777",
+  appId: "1:682393464777:web:f6c7937de964a66befc218"  
+  };
+
+export default firebaseConfig;
